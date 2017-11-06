9472bb72eac70e46916b8ba19c4e61674aca9ff4
Darragh Blake <darragh.blake48@gmail.com>
2017-11-06 15:09:15 +0000

    Added a collectionView within our original collectionView

---
+++ AppStore/CategoryCell.swift
@@ -0,0 +1,45 @@
+//
+//  CategoryCell.swift
+//  AppStore
+//
+//  Created by Darragh on 11/6/17.
+//  Copyright © 2017 Darragh. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // Creating a CollectionView within our current CollectionView
+    let appsCellectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        
+        collectionView.backgroundColor = .blue
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return collectionView
+    }()
+    
+    func setupViews() {
+        backgroundColor = .black
+        
+        // Add collection onto the subview hierarchy
+        addSubview(appsCellectionView)
+        
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": appsCellectionView]))
+        
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": appsCellectionView]))
+    }
+    
+}
