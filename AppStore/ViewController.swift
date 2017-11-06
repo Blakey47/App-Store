028052c4ce885a448642e5d9c0ecd4739ae4a786
Darragh Blake <darragh.blake48@gmail.com>
2017-11-06 14:30:48 +0000

    Dequeue Cells

--- AppStore/ViewController.swift
+++ AppStore/ViewController.swift
@@ -10,12 +10,42 @@ import UIKit
 
 class FeaturedAppsController: UICollectionViewController {
 
+    private let cellId = "cellId"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        collectionView?.backgroundColor = .red
+        collectionView?.backgroundColor = .white
         
+        collectionView?.register(CategoryCell.self, forCellWithReuseIdentifier: cellId)
+    }
+    
+    // Dequeuing the cells
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! CategoryCell
+        return cell
+    }
+    
+    // Creating 3 cells
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
     }
 
 }
 
+class CategoryCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupViews() {
+        backgroundColor = .red
+    }
+    
+}
